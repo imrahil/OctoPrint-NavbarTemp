466fae8777206782d3a2cf1c3e05a638f5e6dd97
Jarek Szczepanski <imrahil@imrahil.com>
2015-05-13 13:01:22 +0200

    Changed formatting. Added target temperature (if set). Should make everyone happy - fix for #4

--- octoprint_navbartemp/static/js/navbartemp.js
+++ octoprint_navbartemp/static/js/navbartemp.js
@@ -11,3 +11,13 @@ $(function() {
         ["#navbar_plugin_navbartemp"]
     ]);
 });
+
+function formatBarTemperature(actual, target) {
+    var output = _.sprintf("%.1f&deg;C", actual);
+
+    if (target) {
+        output += " \u21D7 " + _.sprintf("%.1f&deg;C", target);
+    }
+
+    return output;
+}
