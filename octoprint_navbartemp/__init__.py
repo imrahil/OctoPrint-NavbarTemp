3bd5163005c49e9cd9758581c93f14b7fc5131ae
Jarek Szczepanski <imrahil@imrahil.com>
2015-06-23 19:34:36 +0200

    Added display for internal temperature from Raspberry Pi. Readings are now visible only when printer is connected. Clean up.

--- octoprint_navbartemp/__init__.py
+++ octoprint_navbartemp/__init__.py
@@ -6,9 +6,69 @@ __license__ = "GNU Affero General Public License http://www.gnu.org/licenses/agp
 __copyright__ = "Copyright (C) 2014 Jarek Szczepanski - Released under terms of the AGPLv3 License"
 
 import octoprint.plugin
+from octoprint.util import RepeatedTimer
+import sys
+import re
 
-class NavBarPlugin(octoprint.plugin.TemplatePlugin, octoprint.plugin.AssetPlugin):
-            
+class NavBarPlugin(octoprint.plugin.StartupPlugin,
+                   octoprint.plugin.TemplatePlugin,
+                   octoprint.plugin.AssetPlugin):
+
+    def __init__(self):
+        self.isRaspi = False
+
+    def on_after_startup(self):
+        if sys.platform == "linux2":
+            with open('/proc/cpuinfo', 'r') as infile:
+                    cpuinfo = infile.read()
+            # Match a line like 'Hardware   : BCM2709'
+            match = re.search('^Hardware\s+:\s+(\w+)$', cpuinfo, flags=re.MULTILINE | re.IGNORECASE)
+
+            if not match:
+                # Couldn't find the hardware, assume it isn't a pi.
+                self.isRaspi = False
+            if match.group(1) == 'BCM2708':
+                self._logger.debug("Pi 1")
+                self.isRaspi = True
+            elif match.group(1) == 'BCM2709':
+                self._logger.debug("Pi 2")
+                self.isRaspi = True
+
+            if self.isRaspi:
+                self._logger.debug("Let's start RepeatedTimer!")
+                t = RepeatedTimer(1.0, self.checkRaspiTemp)
+                t.start()
+        # else:
+        #     t = RepeatedTimer(5.0, self.checkRaspiTemp)
+        #     t.start()
+
+        self._logger.debug("is Raspberry Pi? - %s" % self.isRaspi)
+
+    def checkRaspiTemp(self):
+        import sarge
+
+        self._logger.debug("Checking Raspberry Pi internal temperature")
+
+        # if sys.platform == "linux2":
+        p = sarge.run("/opt/vc/bin/vcgencmd measure_temp").returncode
+        # else:
+        #     import random
+        #
+        #     def randrange_float(start, stop, step):
+        #         return random.randint(0, int((stop - start) / step)) * step + start
+        #
+        #     p = "temp=%s'C" % randrange_float(5, 60, 0.1)
+        self._logger.debug("response: %s" % p)
+
+        match = re.search('=(.*)\'', p)
+        if not match:
+            self.isRaspi = False
+        else:
+            temp = match.group(1)
+            self._logger.debug("match: %s" % temp)
+            self._plugin_manager.send_plugin_message(self._identifier, dict(israspi=self.isRaspi, raspitemp=temp))
+
+
     ##~~ AssetPlugin API
     
     def get_assets(self):
--- octoprint_navbartemp/static/js/navbartemp.js
+++ octoprint_navbartemp/static/js/navbartemp.js
@@ -3,6 +3,23 @@ $(function() {
         var self = this;
 
         self.navBarTempModel = parameters[0];
+        self.raspiTemp = ko.observable();
+        self.isRaspi = ko.observable(false);
+
+        self.onDataUpdaterPluginMessage = function(plugin, data) {
+            if (plugin != "navbartemp") {
+                return;
+            }
+
+            if (!data.hasOwnProperty("israspi")) {
+                self.isRaspi(false);
+                return;
+            } else {
+                self.isRaspi(true);
+            }
+
+            self.raspiTemp(_.sprintf("Raspi: %.1f&deg;C", data.raspitemp));
+        };
     }
 
     ADDITIONAL_VIEWMODELS.push([
@@ -12,12 +29,12 @@ $(function() {
     ]);
 });
 
-function formatBarTemperature(actual, target) {
-    var output = _.sprintf("%.1f&deg;C", actual);
+function formatBarTemperature(toolName, actual, target) {
+    var output = toolName + ": " + _.sprintf("%.1f&deg;C", actual);
 
     if (target) {
         output += " \u21D7 " + _.sprintf("%.1f&deg;C", target);
     }
 
     return output;
-}
+};
