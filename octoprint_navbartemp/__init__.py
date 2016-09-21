95f940be5a946f00a95d5612fd7b2c4d1dcaff58
Anton Osenenko <orangeudav@gmail.com>
2016-09-21 14:33:42 +0300

    Add Odroid XU3/XU4 support

--- octoprint_navbartemp/__init__.py
+++ octoprint_navbartemp/__init__.py
@@ -17,42 +17,56 @@ class NavBarPlugin(octoprint.plugin.StartupPlugin,
 
     def __init__(self):
         self.isRaspi = False
+        self.isOdroidXU3 = False
         self.debugMode = False      # to simulate temp on Win/Mac
         self.displayRaspiTemp = True
+        self.displayOdroidXU3Temp = True
         self._checkTempTimer = None
 
     def on_after_startup(self):
         self.displayRaspiTemp = self._settings.get(["displayRaspiTemp"])
-        self._logger.debug("displayRaspiTemp: %s" % self.displayRaspiTemp)
+        self.displayOdroidXU3Temp = self._settings.get(["displayOdroidXU3Temp"])
+        self._logger.debug("displayRaspiTemp: %s, displayOdroidXU3Temp: %s" % (self.displayRaspiTemp, self.displayOdroidXU3Temp))
 
         if sys.platform == "linux2":
             with open('/proc/cpuinfo', 'r') as infile:
                     cpuinfo = infile.read()
             # Match a line like 'Hardware   : BCM2709'
-            match = re.search('^Hardware\s+:\s+(\w+)$', cpuinfo, flags=re.MULTILINE | re.IGNORECASE)
+            match = re.search('^Hardware\s+:\s+([a-zA-Z0-9_-]+)$', cpuinfo, flags=re.MULTILINE | re.IGNORECASE)
 
             if match is None:
                 # Couldn't find the hardware, assume it isn't a pi.
                 self.isRaspi = False
+                self.isOdroidXU3 = False
             elif match.group(1) == 'BCM2708':
                 self._logger.debug("Pi 1")
                 self.isRaspi = True
+                self.isOdroidXU3 = False
             elif match.group(1) == 'BCM2709':
                 self._logger.debug("Pi 2")
                 self.isRaspi = True
+                self.isOdroidXU3 = False
+            elif match.group(1) == 'ODROID-XU3':
+                self._logger.debug("Odroid XU3")
+                self.isRaspi = False
+                self.isOdroidXU3 = True
 
-            if self.isRaspi and self.displayRaspiTemp:
+            if (self.isRaspi and self.displayRaspiTemp) or (self.isOdroidXU3 and self.displayOdroidXU3Temp):
                 self._logger.debug("Let's start RepeatedTimer!")
                 self.startTimer(30.0)
         elif self.debugMode:
             self.isRaspi = True
-            if self.displayRaspiTemp:
+            self.isOdroidXU3 = True
+            if self.displayRaspiTemp or self.displayOdroidXU3Temp:
                 self.startTimer(5.0)
 
-        self._logger.debug("is Raspberry Pi? - %s" % self.isRaspi)
+        self._logger.debug("is Raspberry Pi? - %s, is Odroid XU3? - %s" % (self.isRaspi, self.isOdroidXU3))
 
     def startTimer(self, interval):
-        self._checkTempTimer = RepeatedTimer(interval, self.checkRaspiTemp, None, None, True)
+        if self.isRaspi:
+            self._checkTempTimer = RepeatedTimer(interval, self.checkRaspiTemp, None, None, True)
+        elif self.isOdroidXU3:
+            self._checkTempTimer = RepeatedTimer(interval, self.checkOdroidXU3Temp, None, None, True)
         self._checkTempTimer.start()
 
     def checkRaspiTemp(self):
@@ -80,17 +94,43 @@ class NavBarPlugin(octoprint.plugin.StartupPlugin,
             self._logger.debug("match: %s" % temp)
             self._plugin_manager.send_plugin_message(self._identifier, dict(israspi=self.isRaspi, raspitemp=temp))
 
+    def checkOdroidXU3Temp(self):
+        from sarge import run, Capture
+
+        self._logger.debug("Checking Odroid XU3 internal temperature")
+
+        if sys.platform == "linux2":
+            p = run("cat /sys/devices/virtual/thermal/thermal_zone0/temp", stdout=Capture())
+            p = p.stdout.text
+
+        elif self.debugMode:
+            import random
+            def randrange_float(start, stop, step):
+                return random.randint(0, int((stop - start) / step)) * step + start
+            p = "temp=%s'C" % randrange_float(5, 60, 0.1)
+
+        self._logger.debug("response from sarge: %s" % p)
+
+        match = re.search('(\d+)', p)
+        if not match:
+            self.isOdroidXU3 = False
+        else:
+            temp = str(int(match.group(1)) / 1000.0)
+            self._logger.debug("match: %s" % temp)
+            self._plugin_manager.send_plugin_message(self._identifier, dict(isodroidxu3=self.isOdroidXU3, odroidxu3temp=temp))
 
 	##~~ SettingsPlugin
     def get_settings_defaults(self):
-        return dict(displayRaspiTemp = self.displayRaspiTemp)
+        if self.isRaspi or self.isOdroidXU3:
+            return dict(displayRaspiTemp = self.displayRaspiTemp, displayOdroidXU3Temp = self.displayOdroidXU3Temp)
 
     def on_settings_save(self, data):
         octoprint.plugin.SettingsPlugin.on_settings_save(self, data)
 
         self.displayRaspiTemp = self._settings.get(["displayRaspiTemp"])
+        self.displayOdroidXU3Temp = self._settings.get(["displayOdroidXU3Temp"])
 
-        if self.displayRaspiTemp:
+        if self.displayRaspiTemp or self.displayOdroidXU3Temp:
             interval = 5.0 if self.debugMode else 30.0
             self.startTimer(interval)
         else:
@@ -103,9 +143,9 @@ class NavBarPlugin(octoprint.plugin.StartupPlugin,
 
 	##~~ TemplatePlugin API
     def get_template_configs(self):
-        if self.isRaspi:
+        if self.isRaspi or self.isOdroidXU3:
             return [
-                dict(type="settings", template="navbartemp_settings_raspi.jinja2")
+                dict(type="settings", template="navbartemp_settings.jinja2")
             ]
         else:
             return []
@@ -116,7 +156,7 @@ class NavBarPlugin(octoprint.plugin.StartupPlugin,
             "js": ["js/navbartemp.js"],
             "css": ["css/navbartemp.css"],
             "less": ["less/navbartemp.less"]
-        } 
+        }
 
     ##~~ Softwareupdate hook
     def get_update_information(self):
--- octoprint_navbartemp/static/js/navbartemp.js
+++ octoprint_navbartemp/static/js/navbartemp.js
@@ -5,7 +5,9 @@ $(function() {
         self.navBarTempModel = parameters[0];
         self.global_settings = parameters[1];
         self.raspiTemp = ko.observable();
+        self.odroidXU3Temp = ko.observable();
         self.isRaspi = ko.observable(false);
+        self.isOdroidXU3 = ko.observable(false);
 
         self.onBeforeBinding = function () {
             self.settings = self.global_settings.settings.plugins.navbartemp;
@@ -16,19 +18,23 @@ $(function() {
                 return;
             }
 
-            if (!data.hasOwnProperty("israspi")) {
-                self.isRaspi(false);
-                return;
+            if (data.hasOwnProperty("israspi")) {
+              self.isRaspi(true);
+              self.isOdroidXU3(false);
+              self.raspiTemp(_.sprintf("Raspi: %.1f&deg;C", data.raspitemp));
+            } else if (data.hasOwnProperty("isodroidxu3")) {
+              self.isRaspi(false);
+              self.isOdroidXU3(true);
+              self.odroidXU3Temp(_.sprintf("Odroid XU3: %.1f&deg;C", data.odroidxu3temp));
             } else {
-                self.isRaspi(true);
+              self.isRaspi(false);
+              self.isOdroidXU3(false);
             }
-
-            self.raspiTemp(_.sprintf("Raspi: %.1f&deg;C", data.raspitemp));
         };
     }
 
     ADDITIONAL_VIEWMODELS.push([
-        NavbarTempViewModel, 
+        NavbarTempViewModel,
         ["temperatureViewModel", "settingsViewModel"],
         ["#navbar_plugin_navbartemp", "#settings_plugin_navbartemp"]
     ]);
