85a79f374d8851a7edc7c9b3f3385ab9647ed454
Michal <sentyrz.m@gmail.com>
2018-10-10 18:22:49 +0200

    Added missing files for previous commit

--- README.md
+++ README.md
@@ -8,3 +8,13 @@ For Raspberry Pi users it's possible to display internal temperature (configurab
 ## Setup
 
 Install the plugin using Plugin Manager from Settings
+
+## Need new platform support?
+If you need support for additional platform, please inform us and add such information:
+* How to read temperature
+* How to define platform type
+
+And be ready for testing.
+
+## Change notes:
+v 1.0 - added support for all platforms running under Armbian
--- octoprint_navbartemp/static/js/navbartemp.js
+++ octoprint_navbartemp/static/js/navbartemp.js
@@ -2,45 +2,50 @@ $(function() {
     function NavbarTempViewModel(parameters) {
         var self = this;
 
-        self.navBarTempModel = parameters[0];
+        self.temperatureModel = parameters[0];
         self.global_settings = parameters[1];
-        self.raspiTemp = ko.observable();
-        self.isRaspi = ko.observable(false);
-
-        self.formatBarTemperature = function(toolName, actual, target) {
-            var output = toolName + ": " + _.sprintf("%.1f&deg;C", actual);
-
-            if (target) {
-                var sign = (target >= actual) ? " \u21D7 " : " \u21D8 ";
-                output += sign + _.sprintf("%.1f&deg;C", target);
-            }
-
-            return output;
-        };
-
+        self.socTemp = ko.observable(null);
+        /* 
+         * raspi and awinner should be combined into something like hasSoc in the python
+         * source, there's no need for this part to know or care what the sbc is made of
+         * 
+         */
+        self.isSupported = ko.observable(false);
+        //hassoc should be taken care of in the python source before it gets this far
+        self.hasSoc = ko.pureComputed(function() {
+            return self.isSupported;
+        });
+        
         self.onBeforeBinding = function () {
             self.settings = self.global_settings.settings.plugins.navbartemp;
         };
 
+        self.formatBarTemperature = function(toolName, actual, target) {
+            var output = toolName + ": " + _.sprintf("%.1f&deg;C", actual);
+        
+            if (target) {
+                var sign = (target >= actual) ? " \u21D7 " : " \u21D8 ";
+                output += sign + _.sprintf("%.1f&deg;C", target);
+            }
+        
+            return output;
+        };
+        
         self.onDataUpdaterPluginMessage = function(plugin, data) {
             if (plugin != "navbartemp") {
                 return;
             }
-
-            if (!data.hasOwnProperty("israspi")) {
-                self.isRaspi(false);
-                return;
-            } else {
-                self.isRaspi(true);
+            else {
+                self.isSupported(data.isSupported);
+                self.socTemp(_.sprintf("SoC: %.1f&deg;C", data.soctemp));
             }
-
-            self.raspiTemp(_.sprintf("Raspi: %.1f&deg;C", data.raspitemp));
         };
     }
 
-    ADDITIONAL_VIEWMODELS.push([
-        NavbarTempViewModel, 
-        ["temperatureViewModel", "settingsViewModel"],
-        ["#navbar_plugin_navbartemp", "#settings_plugin_navbartemp"]
-    ]);
+    OCTOPRINT_VIEWMODELS.push({
+        construct: NavbarTempViewModel, 
+        dependencies: ["temperatureViewModel", "settingsViewModel"],
+        elements: ["#navbar_plugin_navbartemp", "#settings_plugin_navbartemp",]
+    });
 });
+
